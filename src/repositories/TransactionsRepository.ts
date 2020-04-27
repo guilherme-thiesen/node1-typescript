a6f43b52a774e8cfe831588589164df128a59954
Guilherme Thiesen <guilhermethiesen@gmail.com>
2020-04-27 01:21:06 -0300

    Desafio 2 Node + TypeScript finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
+import { uuid } from 'uuidv4';
 import Transaction from '../models/Transaction';
 
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface ListTransactionDTO {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -13,16 +25,46 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {
-    // TODO
+  public all(): ListTransactionDTO {
+    const balance = this.getBalance();
+    const transactions = {
+      transactions: this.transactions,
+      balance,
+    };
+
+    return transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const parcialBalance = this.transactions.reduce(
+      (prev, elem) => {
+        // eslint-disable-next-line no-param-reassign
+        prev[elem.type] += elem.value * 1;
+        return prev;
+      },
+      {
+        income: 0,
+        outcome: 0,
+      },
+    );
+
+    const { income, outcome } = parcialBalance;
+    const total = income - outcome;
+
+    const balance: Balance = {
+      income,
+      outcome,
+      total,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = { id: uuid(), title, value, type };
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
